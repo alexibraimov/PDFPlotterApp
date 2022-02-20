d4e8821469b35915e07fbb14285d3690bceb390f
Alexander Ibraimov <59023460+AlexanderIbraimov@users.noreply.github.com>
2022-02-20 20:07:39 +0600

    fix

---
+++ CropHackLib/GhostScriptFacade.cs
@@ -0,0 +1,54 @@
+﻿using Ghostscript.NET;
+using Ghostscript.NET.Rasterizer;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CropHackLib
+{
+    public class GhostScriptFacade
+    {
+        static GhostScriptFacade()
+        {
+            Check();
+        }
+        public static void Check()
+        {
+            if (!GhostscriptVersionInfo.IsGhostscriptInstalled)
+            {
+                throw new Exception("You don't have Ghostscript installed on this machine!");
+            }
+        }
+
+        public static void GetImageFromPdf(string inputPdfPath, string outputImagePath, ImageFormat imageFormat = null)
+        {
+            if (imageFormat == null)
+            {
+                imageFormat = ImageFormat.Png;
+            }
+            int desired_dpi = 80;
+
+            using (GhostscriptRasterizer rasterizer = new GhostscriptRasterizer())
+            {
+                rasterizer.CustomSwitches.Add("-dUseCropBox");
+                rasterizer.CustomSwitches.Add("-c");
+                rasterizer.CustomSwitches.Add("[/CropBox [24 72 559 794] /PAGES pdfmark");
+                rasterizer.CustomSwitches.Add("-f");
+
+                rasterizer.Open(inputPdfPath);
+
+                for (int pageNumber = 1; pageNumber <= rasterizer.PageCount; pageNumber++)
+                {
+                    Image img = rasterizer.GetPage(desired_dpi, pageNumber);
+                    img.Save(outputImagePath, imageFormat);
+                }
+            }
+        }
+    }
+}
